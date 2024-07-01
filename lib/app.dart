d5670f72369dde02b44f42b36d40c0f0f567c5a0
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-07-01 23:30:29 +0600

    color adjust

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:selefe_test/view/screens/home.dart';
 import 'package:get/get.dart';
+import 'package:selefe_test/view/utils/app_colors.dart';
 
 
 class selefe extends StatefulWidget {
@@ -19,11 +20,12 @@ class _selefeState extends State<selefe> {
         designSize: Size(360, 640),
     builder: (context, child)=> GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Task Manager',
+      title: 'Selefe Test',
       theme: ThemeData(
+
           brightness: Brightness.light,
-          primaryColor: Colors.blueAccent,
-          primarySwatch: Colors.blue,
+          primaryColor: AppColors.primarycolor,
+          primarySwatch: Colors.green,
           inputDecorationTheme: const InputDecorationTheme(
             contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
             filled: true,
@@ -43,7 +45,7 @@ class _selefeState extends State<selefe> {
           ),
           elevatedButtonTheme: ElevatedButtonThemeData(
               style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueAccent,
+                  backgroundColor: AppColors.primarycolor,
                   padding: EdgeInsets.symmetric(vertical: 8),
                   elevation: 3,
                   shape: RoundedRectangleBorder(
@@ -53,39 +55,8 @@ class _selefeState extends State<selefe> {
           )
       ),
 
-
-
-      darkTheme: ThemeData(brightness: Brightness.dark,
-          primaryColor: Colors.black,
-          primarySwatch: Colors.blue,
-          inputDecorationTheme: const InputDecorationTheme(
-            contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
-            filled: true,
-            fillColor: Colors.white,
-            hintStyle:  TextStyle(color: Colors.grey),
-            border: OutlineInputBorder(borderSide: BorderSide.none),
-          ),
-          textTheme: TextTheme(
-              titleLarge: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 25,
-                  color: Colors.black,
-                  letterSpacing: 0.5
-              )
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueAccent,
-                  padding: EdgeInsets.symmetric(vertical: 8),
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)
-                  )
-              )
-          )
-      ),
       themeMode: ThemeMode.light,
-      home: const HomeScreen(),
+      home:  HomeScreen(),
       initialBinding: ControllerBinding(),
     )
     );
