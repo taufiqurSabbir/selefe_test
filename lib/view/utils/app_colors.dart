ca3877b5091d85d02f48eb164a30d01aaff590c7
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-07-01 23:30:34 +0600

    color adjust

--- lib/view/utils/app_colors.dart
+++ lib/view/utils/app_colors.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
 class AppColors{
-  static const primarycolor = Color(0xFFfc6d00);
-  static const secondarycolor  = Color(0xFF091253);
+  static const primarycolor = Color(0xFF04612C);
+  static const secondarycolor  = Color(0xFF41775C);
 }
